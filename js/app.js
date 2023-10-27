926838b31964e615f488a99051ec9311932d52f3
roniwellington <roniwellington@gmail.com>
2023-10-27 20:09:02 -0300

    tentando corrigir problema do localStorage

--- js/app.js
+++ js/app.js
@@ -1,128 +1,101 @@
-// let totalGeral;
-// let totaGeral;
-// limpar();
-// limparTotal();
-
-// function adicionarTotal() {
-//     //recuperar valores nome do produto, quantidade de valor
-//     let produtoTotal = document.getElementById('produtoTotal').value;
-//     let nomeProdutoTotal = produtoTotal.split('-')[0];
-//     let valorUnitarioTotal = produtoTotal.split('R$')[1];
-//     let quantidadeTotal = document.getElementById('quantidadeTotal').value;
-//     //calcular o preço, o nosso subtotal
-//     let precoTotal = quantidadeTotal * valorUnitarioTotal;
-//     let carrinhoTotal = document.getElementById('listaProdutosTotal');
-//     //adicionar no carrinho
-//     carrinhoTotal.innerHTML = carrinhoTotal.innerHTML + `<section class="carrinho__produtos__produto">
-//     <span class="texto-azul">${quantidadeTotal}x</span> ${nomeProdutoTotal} <span class="texto-azul">R$${precoTotal}</span>
-//   </section>`
-//     //atualizar o valor total
-//     totalGeral = totalGeral + precoTotal;
-//     let campoTotalDeCaixas = document.getElementById('valorTotal');
-//     campoTotalDeCaixas.textContent = `R$${totalGeral}`;
-//     document.getElementById('quantidadeTotal').value = 0;
-// }
-
-// function limparTotal() {
-//   totalGeral = 0;
-//   document.getElementById('listaProdutosTotal').innerHTML = '';
-//   document.getElementById('valorTotal').textContent = 'R$ 0';
-// }
+let totalGeral;
+let totaGeral;
 
 
-
-// function limpar() {
-//     totaGeral = 0;
-//     document.getElementById('lista-produtos').innerHTML = '';
-//     document.getElementById('valor-total').textContent = 'R$ 0';
-// }
-
-// function adicionar() {
-//   //recuperar valores nome do produto, quantidade de valor
-//   let produto = document.getElementById('produto').value;
-//   let nomeProduto = produto.split('-')[0];
-//   let valorUnitario = produto.split('R$')[1];
-//   let quantidade = document.getElementById('quantidade').value;
-//   //calcular o preço, o nosso subtotal
-//   let preco = quantidade * valorUnitario;
-//   let carrinho = document.getElementById('lista-produtos');
-//   //adicionar no carrinho
-//   carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
-//   <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
-// </section>`
-//   //atualizar o valor total
-//   totaGeral = totaGeral + preco;
-//   let campoTotal = document.getElementById('valor-total');
-//   campoTotal.textContent = `R$${totaGeral}`;
-//   document.getElementById('quantidade').value = 0;
-// }
-
-
-
-let totalGeral = parseFloat(localStorage.getItem('totalGeral')) || 0;
-let totalGeralTotal = parseFloat(localStorage.getItem('totalGeralTotal')) || 0;
+// Novas variáveis para armazenar as chaves e valores
+let produtoTotal = "";
+let nomeProdutoTotal = "";
+let valorUnitarioTotal = "";
+let quantidadeTotal = 0;
+let precoTotal = 0;
 
 limpar();
 limparTotal();
 
+// Função para adicionar um produto ao carrinho
 function adicionarTotal() {
-  let produtoTotal = document.getElementById('produtoTotal').value;
-  let nomeProdutoTotal = produtoTotal.split('-')[0];
-  let valorUnitarioTotal = parseFloat(produtoTotal.split('R$')[1]);
-  let quantidadeTotal = parseInt(document.getElementById('quantidadeTotal').value);
+  // Recuperar os valores do produto
+  produtoTotal = document.getElementById("produtoTotal").value;
+  nomeProdutoTotal = produtoTotal.split("-")[0];
+  valorUnitarioTotal = produtoTotal.split("R$")[1];
+  quantidadeTotal = document.getElementById("quantidadeTotal").value;
 
-  let precoTotal = quantidadeTotal * valorUnitarioTotal;
-  let carrinhoTotal = document.getElementById('listaProdutosTotal');
+  // Calcular o preço
+  precoTotal = quantidadeTotal * valorUnitarioTotal;
 
+  // Adicionar o produto ao carrinho
+  let carrinhoTotal = document.getElementById("listaProdutosTotal");
   carrinhoTotal.innerHTML += `<section class="carrinho__produtos__produto">
-    <span class="texto-azul">${quantidadeTotal}x</span> ${nomeProdutoTotal} <span class="texto-azul">R$${precoTotal.toFixed(2)}</span>
+    <span class="texto-azul">${quantidadeTotal}x</span> ${nomeProdutoTotal} <span class="texto-azul">R$${precoTotal}</span>
   </section>`;
 
-  totalGeralTotal += precoTotal;
-  let campoTotalDeCaixas = document.getElementById('valorTotal');
-  campoTotalDeCaixas.textContent = `R$${totalGeralTotal.toFixed(2)}`;
-  document.getElementById('quantidadeTotal').value = 0;
+  // Atualizar o valor total
+  totalGeral += precoTotal;
+  let campoTotalDeCaixas = document.getElementById("valorTotal");
+  campoTotalDeCaixas.textContent = `R$${totalGeral}`;
+  document.getElementById("quantidadeTotal").value = 0;
 
-  // Atualiza o localStorage
-  localStorage.setItem('totalGeralTotal', totalGeralTotal);
+  // Armazenar os dados no LocalStorage
+  localStorage.setItem("produtoTotal", JSON.stringify(produtoTotal));
+  localStorage.setItem("nomeProdutoTotal", JSON.stringify(nomeProdutoTotal));
+  localStorage.setItem("valorUnitarioTotal",JSON.stringify(valorUnitarioTotal));
+  localStorage.setItem("quantidadeTotal", JSON.stringify(quantidadeTotal));
+  localStorage.setItem("precoTotal", JSON.stringify(precoTotal));
+ 
 }
 
+// Função para limpar o carrinho
 function limparTotal() {
-  totalGeralTotal = 0;
-  document.getElementById('listaProdutosTotal').innerHTML = '';
-  document.getElementById('valorTotal').textContent = 'R$ 0.00';
-
-  // Atualiza o localStorage
-  localStorage.setItem('totalGeralTotal', totalGeralTotal);
-}
-
-function limpar() {
   totalGeral = 0;
-  document.getElementById('lista-produtos').innerHTML = '';
-  document.getElementById('valor-total').textContent = 'R$ 0.00';
+  produtoTotal = "";
+  nomeProdutoTotal = "";
+  valorUnitarioTotal = "";
+  quantidadeTotal = 0;
+  precoTotal = 0;
 
-  // Atualiza o localStorage
-  localStorage.setItem('totalGeral', totalGeral);
+  // Limpar o carrinho
+  let carrinhoTotal = document.getElementById("listaProdutosTotal");
+  carrinhoTotal.innerHTML = "";
+
+  // Limpar o valor total
+  let campoTotalDeCaixas = document.getElementById("valorTotal");
+  campoTotalDeCaixas.textContent = "R$ 0";
+
+  // Remover os dados do LocalStorage
+  localStorage.removeItem("produtoTotal");
+  localStorage.removeItem("nomeProdutoTotal");
+  localStorage.removeItem("valorUnitarioTotal");
+  localStorage.removeItem("quantidadeTotal");
+  localStorage.removeItem("precoTotal");
 }
 
+// Funções para limpar o carrinho e o valor total
+function limpar() {
+  totaGeral = 0;
+  document.getElementById("lista-produtos").innerHTML = "";
+  document.getElementById("valor-total").textContent = "R$ 0";
+}
+
+// Função para adicionar um produto ao carrinho
 function adicionar() {
-  let produto = document.getElementById('produto').value;
-  let nomeProduto = produto.split('-')[0];
-  let valorUnitario = parseFloat(produto.split('R$')[1]);
-  let quantidade = parseInt(document.getElementById('quantidade').value);
+  // Recuperar os valores do produto
+  produto = document.getElementById("produto").value;
+  nomeProduto = produto.split("-")[0];
+  valorUnitario = produto.split("R$")[1];
+  quantidade = document.getElementById("quantidade").value;
 
-  let preco = quantidade * valorUnitario;
-  let carrinho = document.getElementById('lista-produtos');
+  // Calcular o preço
+  preco = quantidade * valorUnitario;
 
+  // Adicionar o produto ao carrinho
+  let carrinho = document.getElementById("lista-produtos");
   carrinho.innerHTML += `<section class="carrinho__produtos__produto">
-    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco.toFixed(2)}</span>
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
   </section>`;
 
-  totalGeral += preco;
-  let campoTotal = document.getElementById('valor-total');
-  campoTotal.textContent = `R$${totalGeral.toFixed(2)}`;
-  document.getElementById('quantidade').value = 0;
-
-  // Atualiza o localStorage
-  localStorage.setItem('totalGeral', totalGeral);
+  // Atualizar o valor total
+  totaGeral += preco;
+  let campoTotal = document.getElementById("valor-total");
+  campoTotal.textContent = `R$${totaGeral}`;
+  document.getElementById("quantidade").value = 0;
 }
